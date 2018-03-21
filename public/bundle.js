af7268a585d6ce0ccbb471a2b3ed529df68c3b3e
dongyang <843408503@qq.com>
2018-03-21 18:16:38 +0800

    第一版代码

---
+++ public/bundle.js
@@ -0,0 +1,68 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function require(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/ 		
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, require);
+/******/ 		
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/ 		
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	require.modules = modules;
+/******/ 	
+/******/ 	// expose the module cache
+/******/ 	require.cache = installedModules;
+/******/ 	
+/******/ 	// __webpack_public_path__
+/******/ 	require.p = "";
+/******/ 	
+/******/ 	
+/******/ 	// Load entry module and return exports
+/******/ 	return require(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, require) {
+
+	
+	const greeter = require(1);
+	document.querySelector("#root").appendChild(greeter());
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, require) {
+
+	// Greeter.js
+	module.exports = function() {
+	  var greet = document.createElement('div');
+	  greet.textContent = "Hi there and greetings!";
+	  return greet;
+	};
+
+
+/***/ }
+/******/ ])
+/*
+//@ sourceMappingURL=bundle.js.map
+*/
